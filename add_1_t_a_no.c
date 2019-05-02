110c1e5e4e55339ac97ccff83474a5621633ba27
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2019-05-02 18:38:19 +0530

    Create add_1_t_a_no.c

---
+++ add_1_t_a_no.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int data()
+{
+    int a;
+    printf("enter a number");
+    scanf("%d",&a);
+    return a;
+}
+int add(int a, int b)
+{
+    int c;
+    c=a+b;
+    return c;
+
+}
+int op(int c)
+{
+    printf("the sum is %d",c);
+}
+int main()
+{
+    int a,b,c;
+   a= data();
+   b= data();
+   c= add(a,b);
+    op(c);
+    return 0;
+}
