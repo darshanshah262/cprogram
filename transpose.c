0ee9812e65221ac77c2f26c4ec3d882889d58c33
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2019-05-03 11:52:22 +0530

    Update transpose.c

--- transpose.c
+++ transpose.c
@@ -12,6 +12,16 @@ void main()
             scanf("%d",&a[i][j]);
         }
     }
+    printf("The entered matrix is :\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+             printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    
     for(i=0;i<n;i++)
     {
         for(j=0;j<n;j++)
