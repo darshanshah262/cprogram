8c2dd39c2814da053bdff1ba62b735a52a45335e
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2019-05-03 10:22:47 +0530

    Create transpose.c

---
+++ transpose.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[10][10],b[10][10],i,j;
+    printf("Enter the size of array that must be a square matrix\n");
+    scanf("%d",&n);
+    printf("Enter the array elements\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            b[i][j]=a[j][i];
+        }
+    }
+    printf("The transposeed matrix is:\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+             printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
+
