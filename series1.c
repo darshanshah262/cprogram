380fb4a8c25f9077fc8d1c94b5d27ef03b928d94
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2019-05-04 15:02:14 +0530

    Create series1.c

---
+++ series1.c
@@ -0,0 +1,19 @@
+/*1
+  12
+  123
+  1234....*/
+  #include<stdio.h>
+  void main()
+  {
+    int i,j,n;
+    printf("Enter the number of rows\n");
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+      for(j=1;j<=i;j++)
+      {
+        printf("  %d  ",j);
+      }
+      printf("\n");
+    }
+  }
