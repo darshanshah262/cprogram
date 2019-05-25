abf3f3f50ae0b2ea41a65a0946c1340a05992a8e
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2019-05-25 23:07:28 +0530

    Create studentdata.c

---
+++ studentdata.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+struct students
+{
+        int rollno;
+        char name[20];
+        float marks;
+        char grade;
+};
+int main()
+{
+        int n,i,rno;
+        printf("Enter the number of students\n");
+        scanf("%d",&n);
+        struct students s[n];
+        for(i=0;i<n;i++)
+        {
+                printf("Enter the %d students roll number, name and marks off 50\n",i+1);
+                scanf("%d",&s[i].rollno);
+                scanf("%s",s[i].name);
+                scanf("%f",&s[i].marks);
+                if(s[i].marks<=10)
+                        s[i].grade='F';
+                else if(s[i].marks>10 && s[i].marks<20)
+                        s[i].grade='D';
+                else if(s[i].marks>=20 && s[i].marks<30)
+                        s[i].grade='C';
+                else if(s[i].marks>=30 && s[i].marks<40)
+                        s[i].grade='B';
+                else
+                        s[i].grade='A';
+        }
+        printf("Enter the student roll number\n");
+        scanf("%d",&rno);
+        for(i=0;i<n;i++)
+        {
+                if(rno==s[i].rollno)
+                {
+                        printf("The students marks and grade are:\n");
+                        printf("Marks  %f\nGrade  %c\n",s[i].marks,s[i].grade);
+                        break;
+                }
+        }
+}
